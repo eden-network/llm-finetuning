755d56f45b9073ded77b6795cce8850b22f0ba18
yoni <jonny@edennetwork.io>
2024-05-21 10:20:55 +0200

    remove commented code in main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from bigquery.writer import async_write
 
 async def async_execute():
     initialize(name="AlfredPros/smart-contracts-instructions", local_name="smart-contracts-instructions", old_input_column="instruction", old_output_column="source_code")
-    # stats = get_stats(dataset="smart-contracts-instructions-mod")    
-    # await async_write(stats)
+    stats = get_stats(dataset="smart-contracts-instructions-mod")    
+    await async_write(stats)
 
 if __name__ == '__main__':
     asyncio.run(async_execute())    
